6e4882d34805eca41d4d2bc8a895e9d08a1eef2e
dkurlyanov <universum220@gmail.com>
2019-12-02 16:31:35 +0500

    test

--- console.cpp
+++ console.cpp
@@ -1,10 +1,53 @@
 #include <iostream>
 #include "school.h"
 
+
+class Base {
+public:
+    int test;
+
+    Base(int test) {
+        this->test = test;
+    }
+
+    int getTest() {
+        return test;
+    }
+};
+
+class PublicChildren : public Base {
+public:
+    int arr[2];
+    int *pArr = new int[2];
+    int *ptr;
+
+    PublicChildren(int test, int arr[]) : Base(test) {
+        //        this->arr = arr;
+        for (int i = 0; i < 1; ++i) {
+            this->arr[i] = arr[i];
+        }
+        *ptr = 1;
+    }
+};
+
+class PrivateChildren : Base {
+public:
+    int test2;
+
+    PrivateChildren(int test, int test2) : Base(test), test2(test2) {
+    }
+};
+
 int main() {
+    int n;
+    int *pArr = new int[n];
+    int arr[2] = {1,2};
+    PublicChildren children = PublicChildren(1,arr);
+
+
     School school = School("school");
 
-    Human student = Human("qwe","123");
+    Human student = Human("qwe", "123");
     school.students.push_back(student);
 
     const string mess = school.getInfo();
